dfd7a49f9e3e1bd72e8798f3918014036f2e74c0
DESKTOP-5PHL67U\Carlos <csi23-cmejero@colegioaltair.net>
2024-05-09 11:58:03 +0200

    no message

--- edu.CMRBootstrap/src/main/webapp/java.js
+++ edu.CMRBootstrap/src/main/webapp/java.js
@@ -1,6 +1,6 @@
 
 // ######## FORMULARIOS ###########
-
+/*
 miUser = document.getElementById("numTarjeta")
 miUser.addEventListener('blur', () => {
 	miUser.value = miUser.value.trim();
@@ -16,7 +16,7 @@ miUser = document.getElementById("codigoSeg")
 miUser.addEventListener('blur', () => {
 	miUser.value = miUser.value.trim();
 })
-
+*/
 
 
 
@@ -79,10 +79,10 @@ function validarNumTarjeta() {
 	if (valorNumTarjeta.length >= 13 && valorNumTarjeta.length <= 18 && Number(valorNumTarjeta)) {
 
 
-		return true;
+		return 1;
 	}
 	else {
-		return false;
+		return 0;
 	}
 
 
@@ -93,11 +93,11 @@ function validarNumTarjeta() {
 function nomTitular() {
 
 	if (NaN(document.getElementById("nomTitular").value)) {
-		return true;
+		return 1;
 
 	}
 	else {
-		return false;
+		return 0;
 	}
 }
 
@@ -107,11 +107,11 @@ function nomTitular() {
 function codSeguridad() {
 	let sdsa = document.getElementById("codigoSeg").value;
 
-	if (Number(sdsa) && sdsa == 4) {
-		return true;
+	if (Number(sdsa) && sdsa > 1000) {
+		return 1;
 	}
 	else {
-		return false;
+		return 0;
 	}
 
 }
@@ -119,13 +119,17 @@ function codSeguridad() {
 
 
 function fechaCaducidad() {
-	let fechaHoy = Date.now();
+	alert("hola")
+	let fechaHoy = new Date();
+	let fechaIngresada = new Date(document.getElementById("fechaCad").value)
 
-	if (document.getElementById("fechaCad").value > fechaHoy) {
-		return true;
+	if (fechaIngresada > fechaHoy) {
+		return 1;
 	}
 	else {
-		return false;
+		alert("La fecha introducida ha caducado")
+		return 0;
+		
 	}
 }
 
@@ -136,7 +140,7 @@ function fechaCaducidad() {
 function maestra2() {
 
 	if (nomTitular() && codigoPostal())
-		return true;
+		return 1;
 
 	else if (!nomTitular) {
 		alert("Has introducido los datos incorrectamente");
@@ -158,51 +162,86 @@ function maestra2() {
 function codigoPostal() {
 
 	if (Number(document.getElementById("codPostal").value) & document.getElementById("codPostal").value.length == 5) {
-		return true;
+		return 1;
 	}
 	else {
-		return false;
+		return 0;
 	}
 }
 
 
 //  ############ ARRAY productos ##########
 
-let arrayJuegos[];
-arrayJuegos.push(["pacific drive" , 17.50]);
-arrayJuegos.push(["horizon" , 41.50]);
-arrayJuegos.push(["forspoken" , 28.02]);
-arrayJuegos.push(["stellar blade" , 69.99]);
-arrayJuegos.push(["f.f. xvi" , 29.50]);
-arrayJuegos.push(["switchback" , 39.99]);
-arrayJuegos.push(["ark ascended" , 39.49]);
-arrayJuegos.push(["dayz" , 27.50]);
-arrayJuegos.push(["rust console" , 39.42]);
-arrayJuegos.push(["conan exiles" , 9.50]);
-arrayJuegos.push(["ark survival" , 12.82]);
-arrayJuegos.push(["battlefield 4" , 11.50]);
-arrayJuegos.push(["spider-man 2" , 62.89]);
-arrayJuegos.push(["cod mw3" , 49.99);
-arrayJuegos.push(["diablo iv" , 79.99]);
-arrayJuegos.push(["the division 2" , 29.50]);
-arrayJuegos.push(["rdd 2" , 17,50]);
-arrayJuegos.push(["gta v" , 10.49]);
-arrayJuegos.push(["tarjeta 50" , 45.00]);
-arrayJuegos.push(["tarjeta 60" , 53.50]);
-arrayJuegos.push(["tarjeta 100" , 92.50]);
-arrayJuegos.push(["ark 2" , 69.50]);
-arrayJuegos.push(["starfield" , 59.00]);
-arrayJuegos.push(["avowed" , 69.99]);
-arrayJuegos.push(["state of decay" , 3.25]);
-arrayJuegos.push(["everwild" , 49.99]);
-arrayJuegos.push(["stalker 2" , 69.99]);
-arrayJuegos.push(["fc 2024" , 47.50]);
-arrayJuegos.push(["tarjeta 3 meses" , 14.49]);
-arrayJuegos.push(["tarjeta 6 meses" , 21.49]);
-arrayJuegos.push(["tarjeta 12 meses" , 42.99]);
-arrayJuegos.push(["elden ring" , 33.33 ]);
-arrayJuegos.push(["blade" , 69.50]);
- console.log(arrayJuegos);
+let arrayJuegos = [];
+arrayJuegos.push(["Pacific Drive", 0, 17.50]);
+arrayJuegos.push(["Horizon", 0, 41.50]);
+arrayJuegos.push(["Forspoken", 0, 28.02]);
+arrayJuegos.push(["Stellar Blade", 0, 69.99]);
+arrayJuegos.push(["F.F. XVI", 0, 29.50]);
+arrayJuegos.push(["Switchback", 0, 39.99]);
+arrayJuegos.push(["Ark Ascended", 0, 39.49]);
+arrayJuegos.push(["Dayz", 0, 27.50]);
+arrayJuegos.push(["Rust Console", 0, 39.42]);
+arrayJuegos.push(["Conan Exiles", 0, 9.50]);
+arrayJuegos.push(["Ark Survival", 0, 12.82]);
+arrayJuegos.push(["Battlefield 4", 0, 11.50]);
+arrayJuegos.push(["Spider-man 2", 0, 62.89]);
+arrayJuegos.push(["cod mw3", 0, 49.99]);
+arrayJuegos.push(["Diablo IV", 0, 79.99]);
+arrayJuegos.push(["The Division 2", 0, 29.50]);
+arrayJuegos.push(["RDD 2", 0, 17.50]);
+arrayJuegos.push(["GTA V", 0, 10.49]);
+arrayJuegos.push(["Tarjeta 10 - PSN", 0, 8.99]);
+arrayJuegos.push(["Tarjeta 20 -PSN", 0, 17.00]);
+arrayJuegos.push(["Tarjeta 25 -PSN", 21, 49.50]);
+arrayJuegos.push(["Tarjeta 50 - PSN", 0, 45.00]);
+arrayJuegos.push(["Tarjeta 60 -PSN", 0, 53.50]);
+arrayJuegos.push(["Tarjeta 100 -PSN", 0, 92.50]);
+arrayJuegos.push(["Ark 2", 0, 69.50]);
+arrayJuegos.push(["Starfield", 0, 59.00]);
+arrayJuegos.push(["Avowed", 0, 69.99]);
+arrayJuegos.push(["State Of Decay", 0, 3.25]);
+arrayJuegos.push(["Everwild", 0, 49.99]);
+arrayJuegos.push(["Stalker 2", 0, 69.99]);
+arrayJuegos.push(["FC 2024", 0, 47.50]);
+arrayJuegos.push(["Tarjeta 3 Meses - GAMEPASS", 0, 14.49]);
+arrayJuegos.push(["Tarjeta 6 Mese - GAMEPASS", 0, 21.49]);
+arrayJuegos.push(["Tarjeta 12 Meses - GAMEPASS", 0, 42.99]);
+arrayJuegos.push(["Elden Ring", 0, 33.33]);
+arrayJuegos.push(["Blade", 0, 69.50]);
+console.log(arrayJuegos);
+
+
+//  ############### CARRITO ##############
+
+let veces = 0;
+
+const dataStored2 = localStorage.getItem("array");
+if (dataStored2) {  
+   arr = JSON.parse(dataStored2)
+}
+if (document.getElementById("contador2"))
+   document.getElementById("contador2").innerHTML = typeof(arrayJuegos)
+
+
+
+
+
+function pulsar(v) {
+   arrayJuegos[v][1]++
+   veces++  
+   localStorage.setItem("array", JSON.stringify(arrayJuegos));
+
+
+}
+function eliminar (y){
+   veces = 0
+   arrayJuegos[y][1]++  
+   localStorage.setItem("array", arrayJuegos)
+}
+
+
+
 
 
 
